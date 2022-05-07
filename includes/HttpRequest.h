d84d42cc6f565de75b4ebae1a4673dd90d4015fa
Zeddmore Ytrams <zytrams@at-e5.msk.21-school.ru>
2022-05-07 21:49:02 +0300

    add getters

--- includes/HttpRequest.h
+++ includes/HttpRequest.h
@@ -6,7 +6,7 @@
 /*   By: zytrams <zytrams@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/07 16:51:15 by zytrams           #+#    #+#             */
-/*   Updated: 2022/05/07 20:29:37 by zytrams          ###   ########.fr       */
+/*   Updated: 2022/05/07 21:44:53 by zytrams          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,67 +17,87 @@
 
 enum HttpMethod
 {
-    GET,
-    POST,
-    DELETE,
-    //PUT,
-    UNKNOWN
+	GET,
+	POST,
+	DELETE,
+	//PUT,
+	UNKNOWN
 };
 
 inline const char* ToString(HttpMethod value)
 {
-    switch (value)
-    {
-        case GET:       return "GET";
-        case POST:      return "POST";
-        case DELETE:    return "DELETE";
-        //case PUT:       return "PUT";
-        default:        return "UNKNOWN";
-    }
+	switch (value)
+	{
+		case GET:       return "GET";
+		case POST:      return "POST";
+		case DELETE:    return "DELETE";
+		//case PUT:       return "PUT";
+		default:        return "UNKNOWN";
+	}
 }
 
 inline HttpMethod ToHttpMethod(std::string value)
 {
-    if (value == "GET")
-        return GET;
-    else if (value == "POST")
-        return POST;
-    else if (value == "DELETE")
-        return DELETE;
-    else
-        return UNKNOWN;
+	if (value == "GET")
+		return GET;
+	else if (value == "POST")
+		return POST;
+	else if (value == "DELETE")
+		return DELETE;
+	else
+		return UNKNOWN;
 }
 
 typedef std::map<std::string, std::string> RequestHeader;
 typedef RequestHeader::iterator header_iterator;
 
 class HttpRequest : private RequestHeader
-{
-    friend class HttpRequestBuilder;
+	{
+	friend class HttpRequestBuilder;
 
-    private:
-        typedef HttpMethod e_http_method;
+	private:
+		typedef HttpMethod e_http_method;
 
-        size_t m_size;
-        std::vector<char> m_body;
-        std::string m_path;
-        std::string m_query;
-        std::string m_version;
-        std::string m_method;
-        bool m_is_valid = false;
-        
-    public:
-        std::string request_address;
+		size_t m_size;
+		std::vector<char> m_body;
+		std::string m_path;
+		std::string m_query;
+		std::string m_version;
+		std::string m_method;
+		bool m_is_valid = false;
 
-        HttpRequest();
-        
-        std::vector<char>& GetBody() 
-        {
-            return m_body;
-        }
+	public:
+		std::string request_address;
 
-        bool IsValid()
-        {
-            return m_is_valid;
-        }
+		HttpRequest();
+
+		std::vector<char>& GetBody() 
+		{
+			return m_body;
+		}
+
+		std::string& GetPath()
+		{
+			return m_path;
+		};
+
+		std::string& GetVersion()
+		{
+			return m_version;
+		};
+
+		std::string& GetMethod()
+		{
+			return m_method;
+		};
+
+		std::string& GetQuery()
+		{
+			return m_query;
+		};
+
+		bool IsValid()
+		{
+			return m_is_valid;
+		}
 };
--- srcs/RequestBuilder.cpp
+++ srcs/RequestBuilder.cpp
@@ -6,7 +6,7 @@
 /*   By: zytrams <zytrams@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/07 18:20:39 by zytrams           #+#    #+#             */
-/*   Updated: 2022/05/07 20:43:28 by zytrams          ###   ########.fr       */
+/*   Updated: 2022/05/07 21:45:24 by zytrams          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 HttpRequestBuilder& HttpRequestBuilder::GetInstance()
 {
-    static HttpRequestBuilder m_instance;
-    return m_instance;
+	static HttpRequestBuilder m_instance;
+	return m_instance;
 }
 
 bool HttpRequestBuilder::ParseInitialFields(HttpRequestBuilder::http_request& req, const std::string msg)
@@ -23,7 +23,7 @@ bool HttpRequestBuilder::ParseInitialFields(HttpRequestBuilder::http_request& re
 	size_t	i, j;
 	std::string	line;
 
-    // METHOD
+	// METHOD
 	i = msg.find_first_of('\n');
 	line = msg.substr(0, i);
 	i = line.find_first_of(' ');
@@ -31,43 +31,43 @@ bool HttpRequestBuilder::ParseInitialFields(HttpRequestBuilder::http_request& re
 	if (i == std::string::npos)
 	{
 		std::cerr << "RFL no space after method" << std::endl;
-        return false;
+		return false;
 	}
-    if (ToHttpMethod(line) == UNKNOWN)
-    {
-        std::cerr << "Invalid method requested" << std::endl;
-        return false;
-    }
-    req.m_method.assign(line, 0, i);
+	if (ToHttpMethod(line) == UNKNOWN)
+	{
+		std::cerr << "Invalid method requested" << std::endl;
+		return false;
+	}
+	req.m_method.assign(line, 0, i);
 
-    // PATH
-    if ((j = line.find_first_not_of(' ', i)) == std::string::npos)
+	// PATH
+	if ((j = line.find_first_not_of(' ', i)) == std::string::npos)
 	{
 		std::cerr << "No PATH / HTTP version" << std::endl;
-        return false;
+		return false;
 	}
 	if ((i = line.find_first_of(' ', j)) == std::string::npos)
 	{
 		std::cerr <<  "No HTTP version" << std::endl;
-        return false;
+		return false;
 	}
-    req.m_path.assign(line, j, i - j);
+	req.m_path.assign(line, j, i - j);
 
-    // VERSION
+	// VERSION
 	if ((i = line.find_first_not_of(' ', i)) == std::string::npos)
 	{
 		std::cerr << "No HTTP version" << std::endl;
-        return false;
+		return false;
 	}
 	if (line[i] == 'H' && line[i + 1] == 'T' && line[i + 2] == 'T' && line[i + 3] == 'P' && line[i + 4] == '/')
 		req.m_version.assign(line, i + 5, 3);
 	if (req.m_version != "1.0" && req.m_version != "1.1")
 	{
 		std::cerr << "BAD HTTP VERSION (" << req.m_version << ")" << std::endl;
-        return false;
+		return false;
 	}
-    
-    return true;
+
+	return true;
 }
 
 std::string HttpRequestBuilder::GetNext(const std::string& msg, size_t& cur)
@@ -80,10 +80,10 @@ std::string HttpRequestBuilder::GetNext(const std::string& msg, size_t& cur)
 	j = msg.find_first_of('\n', cur);
 	ret = msg.substr(cur, j - cur);
 	if (ret[ret.size() - 1] == '\r')
-    {
-        if (ret.size())
-            ret.resize(ret.size() - 1);
-    }
+	{
+		if (ret.size())
+			ret.resize(ret.size() - 1);
+	}
 	cur = (j == std::string::npos ? j : j + 1);
 	return ret;
 }
@@ -121,16 +121,16 @@ void HttpRequestBuilder::ParseValue(std::string& value, const std::string& line)
 
 HttpRequestBuilder::http_request HttpRequestBuilder::BuildHttpRequest(const std::string msg)
 {
-    HttpRequestBuilder::http_request http_req;
-    std::string current;
-    std::string key;
-    std::string value;
-    bool is_valid = true;
-    size_t cur_size = 0;
+	HttpRequestBuilder::http_request http_req;
+	std::string current;
+	std::string key;
+	std::string value;
+	bool is_valid = true;
+	size_t cur_size = 0;
 
-    is_valid = ParseInitialFields(http_req, GetNext(msg, cur_size));
+	is_valid = ParseInitialFields(http_req, GetNext(msg, cur_size));
 
-    while ((current = GetNext(msg, cur_size)) != "\r" && current != "" && is_valid)
+	while ((current = GetNext(msg, cur_size)) != "\r" && current != "" && is_valid)
 	{
 		ParseKey(key, current);
 		ParseValue(value, current);
@@ -140,13 +140,14 @@ HttpRequestBuilder::http_request HttpRequestBuilder::BuildHttpRequest(const std:
 			//TODO CGI
 	}
 
-    if (is_valid)
-    {
-        current = msg.substr(cur_size, std::string::npos);
-        http_req.m_body = std::vector<char>(current.begin(), current.end());
-        GetQuery(http_req);
-    }
+	if (is_valid)
+	{
+		current = msg.substr(cur_size, std::string::npos);
+		http_req.m_body = std::vector<char>(current.begin(), current.end());
+		GetQuery(http_req);
+	}
 
-    http_req.m_is_valid = is_valid;
-    return http_req;
+	http_req.m_is_valid = is_valid;
+	http_req.m_size = msg.size();
+	return http_req;
 }
